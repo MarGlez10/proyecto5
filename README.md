0771604f227ba23ffc556f401fa9f6283fb4414a
MarGlez10 <mcristy.gb@gmail.com>
2024-07-17 20:48:12 -0600

    Actualice el codigo de py y la informacion del prouecto en el archivo readme

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # proyecto5
 Proyecto 5 TripleTen
+En el presente proyecto se pretende desarrollar las habilidades de la creacion y gestion de entornos virtuales de Python, el desarrollo de  una aplicacion web y su despliegue en Render que lo hace accesible al publico. 
+Para dicho proyecto utilice un conjunto de datos que refleja la publicacion de anuncios para ventas de vehiculos. En la aplicacion rmostramos dos graficos, el primero un histograma que refleja la cantidad de anuncios publicados en relacion al modelo del año del vehiculo. En el segundo grafico podemos ver un grafico de dispersion que refleja la relacion del odometro y el precio publicado de los vehiculos. 
--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import plotly.express as px
 import streamlit as st
-st.header('Anuncios de coches')
+st.header('Anuncios de venta de coches')
 
 car_data = pd.read_csv('./vehicles_us.csv')  # leer los datos
 hist_button = st.button('Construir histograma')  # crear un botón
@@ -9,10 +9,10 @@ hist_button = st.button('Construir histograma')  # crear un botón
 if hist_button:  # al hacer clic en el botón
     # escribir un mensaje
     st.write(
-        'Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
+        'Histograma para el numero de anuncios publicados por modelo del año de los vehiculos')
 
     # crear un histograma
-    fig = px.histogram(car_data, x="odometer")
+    fig = px.histogram(car_data, x="model_year")
 
     # mostrar un gráfico Plotly interactivo
     st.plotly_chart(fig, use_container_width=True)
@@ -23,7 +23,7 @@ build_grafdisp = st.checkbox('Construir gráfico de dispersión')
 if build_grafdisp:  # al hacer clic en el botón
     # escribir un mensaje
     st.write(
-        'Crea un gráfico de dispersión para el conjunto de datos de anuncios de venta de coches')
+        'Gráfico de dispersión para el conjunto de datos de anuncios de venta de coches')
 
     # crear un gráfico de dispersión
     build_grafdisp = px.scatter(car_data, x='odometer', y='price')
